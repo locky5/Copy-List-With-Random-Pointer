0967642ab7700400c1575c6ca0bd79777de03037
locky5 <daikevin19@yahoo.com>
2020-01-01 15:57:53 -0500

    done

---
+++ copylistrandompointer.js
@@ -0,0 +1,19 @@
+var copyRandomList = function(head) {
+    let dic = new Map()
+    dic.set(null, null)
+
+    let curr = head
+    while (curr) {
+        dic.set(curr, new Node(curr.val, null, null))
+        curr = curr.next
+    }
+
+    curr = head
+    while (curr) {
+        dic.get(curr).next = dic.get(curr.next)
+        dic.get(curr).random = dic.get(curr.random)
+        curr = curr.next
+    }
+
+    return dic.get(head)
+};
